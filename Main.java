d0848a976a01622dc7daabbd1f58b1cbf549c18a
SashaDch <sashadychko@mail.ru>
2025-05-12 20:53:39 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PasswordChecker passwordChecker = new PasswordChecker();
+        String userInput;
+
+        while (true) {
+            System.out.print("\nВведите мин. длину пароля: ");
+            userInput = scanner.nextLine();
+            if (userInput.equals("end")) {
+                System.out.println("\nПрограмма завершена");
+                return;
+            }
+            try {
+                passwordChecker.setMinLength(Integer.parseInt(userInput));
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный формат числа: " + e);
+                continue;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            System.out.print("\nВведите макс. допустимое количество повторений символа подряд: ");
+            userInput = scanner.nextLine();
+            if (userInput.equals("end")) {
+                System.out.println("\nПрограмма завершена");
+                return;
+            }
+            try {
+                passwordChecker.setMaxRepeats(Integer.parseInt(userInput));
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный формат числа: " + e);
+                continue;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            System.out.print("\nВведите пароль или end: ");
+            userInput = scanner.nextLine();
+            if (userInput.equals("end")) {
+                System.out.println("\nПрограмма завершена");
+                return;
+            }
+            try {
+                if (passwordChecker.verify(userInput)) {
+                    System.out.println("Подходит!");
+                } else  {
+                    System.out.println("Не подходит!");
+                }
+            } catch (IllegalStateException e) {
+                System.out.println(e);
+                return;
+            }
+        }
+    }
+}
---
+++ PasswordChecker.java
@@ -0,0 +1,47 @@
+public class PasswordChecker {
+    private Integer minLength;
+
+    private Integer maxRepeats;
+
+
+    public void setMinLength(int minLength) throws IllegalArgumentException {
+        if (minLength < 0) {
+            throw new IllegalArgumentException("Parameter minLength must be non-negative!");
+        }
+        this.minLength = minLength;
+    }
+
+    public void setMaxRepeats(int maxRepeats) throws IllegalArgumentException {
+        if (maxRepeats <= 0) {
+            throw new IllegalArgumentException("Parameter maxRepeats must be positive!");
+        }
+        this.maxRepeats = maxRepeats;
+    }
+
+    public boolean verify(String password) throws IllegalStateException {
+        if (minLength == null || maxRepeats == null) {
+            throw new IllegalStateException("Some parameters is not set: " +
+                    (minLength == null ? "minLength" : "") +
+                    (minLength == null && maxRepeats == null ? ", " : "") +
+                    (maxRepeats == null ? "maxRepeats" : "") +
+                    "!");
+        }
+        return password.length() >= this.minLength && repeatsNotExceeded(password, this.maxRepeats);
+    }
+
+    private static boolean repeatsNotExceeded(String string, int repeats) {
+        int n = 1;
+
+        for (int i = 1; i < string.length(); i++) {
+            if (string.charAt(i) == string.charAt(i - 1)) {
+                n++;
+            } else {
+                n = 1;
+            }
+            if (n > repeats) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
